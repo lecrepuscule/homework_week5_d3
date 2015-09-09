046dadde387f043c789ecda001253940c6480bba
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-09 17:35:20 +0100

    first commit

---
+++ games-inventory/app.rb
@@ -0,0 +1,24 @@
+class GameSpot < Sinatra::Base
+
+  # General route actions
+  get "/" do
+    
+  end
+
+  # RESTful Artist Controller Actions
+
+  # index
+  
+  # new
+ 
+  # create
+  
+  # show
+
+  # edit
+
+  # update
+  
+  # delete
+
+end
---
+++ games-inventory/config.ru
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+# Controllers
+require './models/game'
+require './app'
+
+run GameSpot
